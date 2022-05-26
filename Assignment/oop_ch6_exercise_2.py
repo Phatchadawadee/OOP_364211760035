e52c721da74f975c1dece8480f23ca018b4f98a8
Phatchadawadee <phatchadawadee.s@rmutsvmail.com>
2022-05-26 16:33:27 +0700

    Lab7

--- Assignment/oop_ch6_exercise_2.py
+++ Assignment/oop_ch6_exercise_2.py
@@ -8,43 +8,27 @@ OOP Exercise Chapter 6
 ความรเร็วสูงสุด (max speed)
 ราคา (price)
 
-2.จากข้อที่ 1 เขียนโปรแกรมเพื่อสร้างวัตถุ (object) จากคลาส Vehicle โดยรับข้อมูลจากผู้ใช้ตามคุณลักษณะ (attributes)ของคลาส
+2.จากข้อที่ 1 เขียนโปรแกรมเพื่อสร้างวัตถุ (object) จากคลาส Vehicle โดยรับข้อมูลจากผู้ใช้ตามคุณลักษณะ (attributes) ของคลาส
 จากนั้นแสดงข้อมูลทางหน้าจอภาพ
 
 """
-from Vehicle import Vehicle
-
-Vehicle_store = []
-num = int(input('คุณมีรถกี่คัน?:'))
-
-for x in range(num):
-    brand = input('ยี่ห้อรถ:')
-    model = input('รุ่นรถ:')
-    color = input('สีรถ:')
-    max_speed = input('ความรเร็วสูงสุด:')
-    price = float(input('ราคา:'))
-
-    a = Vehicle(brand,model,color,max_speed,price)
-    Vehicle_store.append(a)
-
-def display_Vehicle(Vehicle):
-    print('จำนวนรถทั้งหมด:',len(Vehicle))
-    for x in Vehicle:
-        x.Vehicle_detail()
-
-display_Vehicle(Vehicle_store)
-
 
 
 class Vehicle:
-    def __init__(self,brandname,model,color,max_speed,price):
+    def __init__(self,bran,color,maxspeed,price):
         # object attributes
-        self.brandname = brandname
-        self.model = model
+        self.brand = bran
         self.color = color
-        self.max_speed = max_speed
-        self.price = price
+        self.maxs = maxspeed  # int
+        self.price = price  # float
 
-    def Vehicle_detail(self):
-        print(f'brandname:{self.brandname} model: {self.model} '
-              f'color:{self.color} max_speed:{self.max_speed} price: {self.price}  THB')
+    def displayDetail(self):
+        print(f'f{self.brand} {self.color} {self.maxs} {self.price}')
+
+b = input('Vehicel brand: ')
+c = input('Color: ')
+m = int(input('Max speed: '))
+p = float(input('Price: '))
+
+v = Vehicle(b,c,m,p)
+v.displayDetail()
